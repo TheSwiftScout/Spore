5125a4152e1666b3730375af9342a0bfb099b10c
TheSwiftScout <143136172+TheSwiftScout@users.noreply.github.com>
2023-08-15 00:33:41 +0200

    Configure Serilog

--- src/Cultivator.Desktop/Program.cs
+++ src/Cultivator.Desktop/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using Avalonia;
 using Avalonia.ReactiveUI;
+using Serilog;
+using Serilog.Events;
+using Splat;
+using Splat.Serilog;
 
 namespace Cultivator.Desktop;
 
@@ -10,8 +14,37 @@ class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        // Serilog
+
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Debug()
+            .WriteTo.Console()
+            .WriteTo.File(
+                "log.txt",
+                LogEventLevel.Information,
+                rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+
+        Locator.CurrentMutable.UseSerilogFullLogger();
+
+        try
+        {
+            // Startup
+            BuildAvaloniaApp()
+                .StartWithClassicDesktopLifetime(args);
+        }
+        catch (Exception ex)
+        {
+            Log.Logger.Fatal(ex, "An unexpected error has occurred and the application will now exit.");
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
