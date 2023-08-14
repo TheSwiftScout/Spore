5558e56be9cbfc73fd47f4029cf77372130fc3c7
TheSwiftScout <143136172+TheSwiftScout@users.noreply.github.com>
2023-08-15 00:21:13 +0200

    Fix ViewLocator warnings

--- src/Cultivator/ViewLocator.cs
+++ src/Cultivator/ViewLocator.cs
@@ -7,20 +7,20 @@ namespace Cultivator;
 
 public class ViewLocator : IDataTemplate
 {
-    public Control Build(object data)
+    public Control Build(object? data)
     {
         if (data is null)
-            return null;
+            throw new ArgumentNullException(nameof(data));
 
         var name = data.GetType().FullName!.Replace("ViewModel", "View");
         var type = Type.GetType(name);
 
-        if (type != null)
+        if (type is not null)
         {
             return (Control)Activator.CreateInstance(type)!;
         }
-        
-        return new TextBlock { Text = name };
+
+        return new TextBlock { Text = $"View not found: {name}" };
     }
 
     public bool Match(object? data)
