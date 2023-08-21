7d1e4b89ba1ab2d8e335ffdcd8a1acef53921a04
TheSwiftScout <143136172+TheSwiftScout@users.noreply.github.com>
2023-08-21 05:10:22 +0200

    Add Gazelle API keys to state

--- src/Cultivator/Main/MainState.cs
+++ src/Cultivator/Main/MainState.cs
@@ -7,4 +7,8 @@ namespace Cultivator.Main;
 public class MainState
 {
     [DataMember] public QBittorrentState QBittorrentState { get; set; } = new();
+
+    [DataMember] public string? RedactedApiKey { get; set; }
+
+    [DataMember] public string? OrpheusApiKey { get; set; }
 }
