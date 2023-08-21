ababdbda1337cdeb2558c671b2ac9d52b803ef66
TheSwiftScout <143136172+TheSwiftScout@users.noreply.github.com>
2023-08-21 04:26:43 +0200

    Add circuit breaker to TransientHttpErrorHandler

--- src/Cultivator/TransientHttpErrorHandler.cs
+++ src/Cultivator/TransientHttpErrorHandler.cs
@@ -6,15 +6,15 @@ using System.Threading.Tasks;
 using Polly;
 using Polly.Contrib.WaitAndRetry;
 using Polly.Extensions.Http;
-using Polly.Retry;
 
 namespace Cultivator;
 
 public class TransientHttpErrorHandler : DelegatingHandler
 {
-    private readonly AsyncRetryPolicy<HttpResponseMessage> _retryPolicy;
+    private readonly AsyncPolicy<HttpResponseMessage> _policy;
 
-    public TransientHttpErrorHandler(HttpMessageHandler? innerHandler = null) : base(innerHandler ?? new HttpClientHandler())
+    public TransientHttpErrorHandler(HttpMessageHandler? innerHandler = null)
+        : base(innerHandler ?? new HttpClientHandler())
     {
         var maxDelay = TimeSpan.FromSeconds(45);
 
@@ -22,16 +22,22 @@ public class TransientHttpErrorHandler : DelegatingHandler
             .DecorrelatedJitterBackoffV2(TimeSpan.FromSeconds(1), 50)
             .Select(delay => TimeSpan.FromTicks(Math.Min(delay.Ticks, maxDelay.Ticks)));
 
-        _retryPolicy = HttpPolicyExtensions
+        var retryPolicy = HttpPolicyExtensions
             .HandleTransientHttpError()
             .WaitAndRetryAsync(delay);
+
+        var circuitBreaker = HttpPolicyExtensions
+            .HandleTransientHttpError()
+            .CircuitBreakerAsync(5, TimeSpan.FromMinutes(1));
+
+        _policy = Policy.WrapAsync(retryPolicy, circuitBreaker);
     }
 
     protected override async Task<HttpResponseMessage> SendAsync(
         HttpRequestMessage request,
         CancellationToken cancellationToken)
     {
-        return await _retryPolicy.ExecuteAsync(
+        return await _policy.ExecuteAsync(
             async innerCancellationToken => await base.SendAsync(request, innerCancellationToken),
             cancellationToken);
     }
