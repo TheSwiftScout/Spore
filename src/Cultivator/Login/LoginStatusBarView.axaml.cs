a2d6a6df6c818c2ad57a30b498a8e0764e96385f
TheSwiftScout <143136172+TheSwiftScout@users.noreply.github.com>
2023-08-22 23:26:09 +0200

    Implement RED/OPS for LoginStatusBar

--- src/Cultivator/Login/LoginStatusBarView.axaml.cs
+++ src/Cultivator/Login/LoginStatusBarView.axaml.cs
@@ -14,9 +14,24 @@ public partial class LoginStatusBarView : ReactiveUserControl<LoginStatusBarView
         this.WhenActivated(disposables =>
         {
             this.WhenAnyObservable(v => v.ViewModel.QBittorrentClient.IsAuthenticated)
-                .Select(auth => auth ? "Connected" : "Disconnected")
+                .Select(IsAuthenticatedToString)
                 .BindTo(this, v => v.QBittorrentStatus.Text)
                 .DisposeWith(disposables);
+
+            this.WhenAnyObservable(v => v.ViewModel.RedactedClient.IsAuthenticated)
+                .Select(IsAuthenticatedToString)
+                .BindTo(this, v => v.RedactedStatus.Text)
+                .DisposeWith(disposables);
+
+            this.WhenAnyObservable(v => v.ViewModel.OrpheusClient.IsAuthenticated)
+                .Select(IsAuthenticatedToString)
+                .BindTo(this, v => v.OrpheusStatus.Text)
+                .DisposeWith(disposables);
         });
+    }
+
+    private static string IsAuthenticatedToString(bool isAuthenticated)
+    {
+        return isAuthenticated ? "Connected" : "Disconnected";
     }
 }
--- src/Cultivator/Login/LoginStatusBarViewModel.cs
+++ src/Cultivator/Login/LoginStatusBarViewModel.cs
@@ -1,13 +1,21 @@
-﻿using Cultivator.QBittorrent;
+﻿using Cultivator.Gazelle;
+using Cultivator.QBittorrent;
 
 namespace Cultivator.Login;
 
 public class LoginStatusBarViewModel : ViewModelBase
 {
-    public LoginStatusBarViewModel(QBittorrentClient qBittorrentClient)
+    public LoginStatusBarViewModel(
+        QBittorrentClient qBittorrentClient,
+        RedactedClient redactedClient,
+        OrpheusClient orpheusClient)
     {
         QBittorrentClient = qBittorrentClient;
+        RedactedClient = redactedClient;
+        OrpheusClient = orpheusClient;
     }
 
     public QBittorrentClient QBittorrentClient { get; }
+    public RedactedClient RedactedClient { get; }
+    public OrpheusClient OrpheusClient { get; }
 }
